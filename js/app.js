bb3c009711d7a03a89bfe53b818dccce151d2d03
Luna-bf <lburattifayolle@gmail.com>
2025-05-13 16:31:47 +0200

    Added the js logic + link in HTML

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 'use strict';
 
+const elements = {
+    allNames: document.querySelector(".background"),
+    form: document.querySelector(".champ"),
+    inputValue: document.getElementById('champ'),
+    submitBtn: document.querySelector(".valider"),
+};
+
 //Le tableau qui va contenir la liste des prénoms
 let names = [];
+
+function addNames() {
+    let template = names.map(name => `<li>${name}</li>`).join('\n');
+    elements.allNames.innerHTML = template;
+    addNames();
+}
+
+elements.submitBtn.addEventListener("click", (e) => {
+    e.preventDefault; //J'empêche le rechargement de la page (comportement par défaut lorsqu'un formulaire est soumis)
+
+    names.push(elements.inputValue.value);
+    elements.inputValue.value = '';
+    addNames();
+});
